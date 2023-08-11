0bfad19d1499b08c819ea9a836ba37c93c1f4c06
Hriday <hridayjadhav789@gmail.com>
2023-08-11 18:50:29 +0530

    arrow function

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+    // Arrow function
+// let sumOfTwoNumbers = (a, b) => a + b;
+// console.log(sumOfTwoNumbers(10, 30)); 
+
+
+
+
+
+// const sumOfTwoNumbers = (a,b) => a * b;
+// console.log(sumOfTwoNumbers(8,9));
+
+var sumOfTwoNumbers = (a,b) => a * b;
+console.log(sumOfTwoNumbers(8,9));
